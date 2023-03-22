e4672be20ef512709f41acf26685d49ae321773d
LanSvetyk <lansvetyk@gmail.com>
2023-03-22 09:05:34 +0300

    changes

--- src/js/components/validation.js
+++ src/js/components/validation.js
@@ -1,110 +1,113 @@
 (function () {
-if (window.location.toString().indexOf('index.html')>0 || window.location.href === "http://localhost:3000/" || window.location.href === "https://lansvetyk.github.io/SitDownPls_prod/" ) {
-  document.addEventListener("DOMContentLoaded", () => {
-    const $form = document.getElementById("form");
-    const $accept = document.getElementById("accept");
-    const regExpName = /^[А-Яа-яЁё\s-]{2,18}$/;
-    const regExpPhone = /^[\d\+][\d\(\)\ -]{9,14}\d$/;
-    const regExpEmail = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i;
-    function validation(form) {
-      function removeError(input) {
-        const parents = input.parentNode;
+  if (
+    window.location.toString().indexOf("index.html") > 0 ||
+    window.location.toString().indexOf("localhost") > 0 ||
+    window.location.href == "https://lansvetyk.github.io/SitDownPls_prod/"
+  ) {
+    document.addEventListener("DOMContentLoaded", () => {
+      const $form = document.getElementById("form");
+      const $accept = document.getElementById("accept");
+      const regExpName = /^[А-Яа-яЁё\s-]{2,18}$/;
+      const regExpPhone = /^[\d\+][\d\(\)\ -]{9,14}\d$/;
+      const regExpEmail = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i;
+      function validation(form) {
+        function removeError(input) {
+          const parents = input.parentNode;
 
-        if (parents.classList.contains("error")) {
-          parents.querySelector(".error-label").remove();
-          parents.classList.remove("error");
-        }
-      }
-      let result = true;
-
-      function createError(input, text) {
-        const parents = input.parentNode;
-        const errorLabel = document.createElement("label");
-
-        errorLabel.classList.add("error-label");
-        errorLabel.textContent = text;
-        parents.prepend(errorLabel);
-        parents.classList.add("error");
-      }
-
-      const allInputs = $form.querySelectorAll("input");
-      for (const input of allInputs) {
-        if (input.value == "") {
-          removeError(input);
-          createError(input, "Заполните поле!");
-          result = false;
-        }
-        if (input.name == "name") {
-          removeError(input);
-          if (!regExpName.test(input.value)) {
-            createError(
-              input,
-              "Введите имя на русском языке от 2 до 18 символов"
-            );
-            result = false;
+          if (parents.classList.contains("error")) {
+            parents.querySelector(".error-label").remove();
+            parents.classList.remove("error");
           }
         }
-        if (input.name == "phone") {
-          removeError(input);
-          if (!regExpPhone.test(input.value)) {
-            createError(input, "Введите номер телефона");
+        let result = true;
+
+        function createError(input, text) {
+          const parents = input.parentNode;
+          const errorLabel = document.createElement("label");
+
+          errorLabel.classList.add("error-label");
+          errorLabel.textContent = text;
+          parents.prepend(errorLabel);
+          parents.classList.add("error");
+        }
+
+        const allInputs = $form.querySelectorAll("input");
+        for (const input of allInputs) {
+          if (input.value == "") {
+            removeError(input);
+            createError(input, "Заполните поле!");
             result = false;
           }
-        }
-        if (input.name == "email") {
-          removeError(input);
-          if (!regExpEmail.test(input.value)) {
-            createError(input, "Введите email");
-            result = false;
+          if (input.name == "name") {
+            removeError(input);
+            if (!regExpName.test(input.value)) {
+              createError(
+                input,
+                "Введите имя на русском языке от 2 до 18 символов"
+              );
+              result = false;
+            }
+          }
+          if (input.name == "phone") {
+            removeError(input);
+            if (!regExpPhone.test(input.value)) {
+              createError(input, "Введите номер телефона");
+              result = false;
+            }
+          }
+          if (input.name == "email") {
+            removeError(input);
+            if (!regExpEmail.test(input.value)) {
+              createError(input, "Введите email");
+              result = false;
+            }
+          }
+          if (input.name == "accept") {
+            removeError(input);
+            if (!$accept.checked) {
+              createError(input, "Согласитесь с условиями");
+              result = false;
+            }
           }
         }
-        if (input.name == "accept") {
-          removeError(input);
-          if (!$accept.checked) {
-            createError(input, "Согласитесь с условиями");
-            result = false;
-          }
-        }
+        return result;
       }
-      return result;
-    }
-    const searchInputText = $form.querySelector('input[type="text"]');
-    searchInputText.addEventListener("keypress", (event) => {
-      let regExp = /[а-яА-Я\s-]/;
-      if (event.key.match(regExp)) {
-        return null;
-      } else {
-        event.preventDefault();
-      }
+      const searchInputText = $form.querySelector('input[type="text"]');
+      searchInputText.addEventListener("keypress", (event) => {
+        let regExp = /[а-яА-Я\s-]/;
+        if (event.key.match(regExp)) {
+          return null;
+        } else {
+          event.preventDefault();
+        }
+      });
+      searchInputText.addEventListener("blur", () => {
+        let correctInput = field.value.trim();
+        while (correctInput.indexOf("-") === 0) {
+          correctInput = correctInput.slice(1);
+        }
+        while (correctInput.substring(correctInput.length - 1) === "-") {
+          correctInput = correctInput.slice(0, -1);
+        }
+        correctInput = correctInput.replace(/-+/g, "-");
+        correctInput = correctInput.replace(/\s+/g, " ");
+        correctInput =
+          correctInput.substring(0, 1).toUpperCase() +
+          correctInput.substring(1, correctInput.length).toLowerCase();
+        field.value = correctInput;
+      });
+      $form.addEventListener("submit", (el) => {
+        el.preventDefault();
+
+        if (validation($form)) {
+          alert("Форма отправлена!");
+          $form.querySelectorAll(".form input").forEach((elem) => {
+            elem.value = "";
+            elem.checked = false;
+            $form.reset();
+          });
+        }
+      });
     });
-    searchInputText.addEventListener("blur", () => {
-      let correctInput = field.value.trim();
-      while (correctInput.indexOf("-") === 0) {
-        correctInput = correctInput.slice(1);
-      }
-      while (correctInput.substring(correctInput.length - 1) === "-") {
-        correctInput = correctInput.slice(0, -1);
-      }
-      correctInput = correctInput.replace(/-+/g, "-");
-      correctInput = correctInput.replace(/\s+/g, " ");
-      correctInput =
-        correctInput.substring(0, 1).toUpperCase() +
-        correctInput.substring(1, correctInput.length).toLowerCase();
-      field.value = correctInput;
-    });
-    $form.addEventListener("submit", (el) => {
-      el.preventDefault();
-
-      if (validation($form)) {
-        alert("Форма отправлена!");
-        $form.querySelectorAll(".form input").forEach((elem) => {
-          elem.value = "";
-          elem.checked = false;
-          $form.reset();
-        });
-      }
-    });
-  });
-}
+  }
 })();
-
